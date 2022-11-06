a31122f4291e78db2ecf7f9f6dff302d38226a4e
deenMuhammad <zorro199696@gmail.com>
2022-11-06 20:09:37 +0900

    First class done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useMutation, useSubscription } from "@apollo/client";
+import { useEffect } from "react";
+const MUTATE_FIRE_EVENT = gql`
+  mutation fireEvent($eventType: String!) {
+    fireEvent(eventType: $eventType)
+  }
+`;
+const SUBSCRIBE_FIRE_EVENT = gql`
+  subscription {
+    onEvent {
+      eventType
+      date
+    }
+  }
+`;
 function App() {
+  const [fireEvent, { data, loading, error }] = useMutation(MUTATE_FIRE_EVENT);
+  const { data: sub_data, loading: sub_loading } = useSubscription(SUBSCRIBE_FIRE_EVENT);
+  useEffect(()=>{
+    if(!sub_loading){
+      console.log('%cApp.js line:22 sub_data', 'color: #007acc;', sub_data);
+    }
+    else{
+      console.log('%cApp.js line:26 sub_loading', 'color: #007acc;', sub_loading);
+    }
+  }, [sub_data])
+  const mutate = () => {
+    fireEvent({ variables: { eventType: "Fired the event" } });
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +36,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          onClick={mutate}
+          style={{
+            width: 100,
+            height: 100,
+            background: "red",
+            cursor: "pointer",
+          }}
         >
-          Learn React
-        </a>
+          Mutate
+        </div>
+        {data && <div>{JSON.stringify(data)}</div>}
       </header>
     </div>
   );
