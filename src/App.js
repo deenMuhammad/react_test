798fec04ab16de3d34879696f6213e877816f1e3
deenMuhammad <zorro199696@gmail.com>
2022-11-19 23:04:33 +0900

    Added message

--- src/App.js
+++ src/App.js
@@ -1,54 +1,73 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { gql, useMutation, useSubscription } from "@apollo/client";
-import { useEffect } from "react";
-const MUTATE_FIRE_EVENT = gql`
-  mutation fireEvent($eventType: String!) {
-    fireEvent(eventType: $eventType)
+import { useEffect, useState } from "react";
+import uuid from "react-uuid";
+const SEND_MESSAGE = gql`
+  mutation setHello($message: String!, $chatId: String!, $clientId: String!) {
+    setHello(message: $message, chatId: $chatId, clientId: $clientId)
   }
 `;
-const SUBSCRIBE_FIRE_EVENT = gql`
-  subscription {
-    onEvent {
-      eventType
+const SUB_TO_CHAT = gql`
+  subscription SubToChat($chatId: String!) {
+    subToChat(chatId: $chatId) {
+      message
+      clientId
       date
     }
   }
 `;
 function App() {
-  const [fireEvent, { data, loading, error }] = useMutation(MUTATE_FIRE_EVENT);
-  const { data: sub_data, loading: sub_loading } = useSubscription(SUBSCRIBE_FIRE_EVENT);
-  useEffect(()=>{
-    if(!sub_loading){
-      console.log('%cApp.js line:22 sub_data', 'color: #007acc;', sub_data);
+  const [clientId, setClientId] = useState("");
+  useEffect(() => {
+    setClientId(uuid());
+  }, []);
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const [sendMessage, { data, loading, error }] = useMutation(SEND_MESSAGE);
+  const {
+    data: sub_data,
+    loading: sub_loading,
+    error: sub_error,
+  } = useSubscription(SUB_TO_CHAT, { variables: { chatId: "123" } });
+  useEffect(() => {
+    if (!sub_loading && !sub_error && sub_data) {
+      //save message to messages state
+      setMessages([...messages, sub_data.subToChat]);
+    } else {
+      console.log(
+        "%cApp.js line:26 sub_loading",
+        "color: #007acc;",
+        sub_loading
+      );
     }
-    else{
-      console.log('%cApp.js line:26 sub_loading', 'color: #007acc;', sub_loading);
-    }
-  }, [sub_data])
-  const mutate = () => {
-    fireEvent({ variables: { eventType: "Fired the event" } });
-  };
+  }, [sub_data]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div
-          onClick={mutate}
-          style={{
-            width: 100,
-            height: 100,
-            background: "red",
-            cursor: "pointer",
-          }}
-        >
-          Mutate
+      <div className="App-header">
+        <div>
+          {messages.map((item, index) => (
+            <div>
+              {" "}
+              {item.message}, {item.date}{" "}
+            </div>
+          ))}
+          <input
+            className="message"
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+          <div
+            className="send"
+            onClick={() => {
+              sendMessage({ variables: { clientId, message, chatId: "123" } });
+            }}
+          >
+            Send
+          </div>
         </div>
-        {data && <div>{JSON.stringify(data)}</div>}
-      </header>
+      </div>
     </div>
   );
 }
--- src/graphql.js
+++ src/graphql.js
@@ -9,7 +9,7 @@ const httpLink = new HttpLink({
 });
 
 const wsLink = new GraphQLWsLink(createClient({
-  url: 'ws://localhost:4000/subscriptions',
+  url: 'ws://localhost:4000/subscription',
 }));
 
 // The split function takes three parameters:
